f83443a2a06cfd1bb72df1782a01ac0cbdc0d9fa
sahilsahu246 <sahilsahu19970@gmail.com>
2026-06-01 19:17:04 +0530

    checking change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Hello from Sahil the CI/CD pipeline creator! Served by host: {socket.gethostname()}\n"
+    return f"Hello change from Sahil the CI/CD pipeline creator! Served by host: {socket.gethostname()}\n"
 
 @app.route("/health")
 def health():
